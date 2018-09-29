d6aeab2b57b06302f97045182d18673f45420909
verminniu <cattlenzq@163.com>
2018-09-29 19:11:44 +0800

    fix: cquery

--- custom/setup-cquery.el
+++ custom/setup-cquery.el
@@ -1,6 +1,4 @@
-(use-package cquery
-  :init
-  )
+(require-package 'cquery)
 
 ;; company
 (use-package company
@@ -39,7 +37,7 @@
 ;; Cache directory, both relative and absolute paths are supported
 (setq cquery-cache-dir "~/.cquery/cquery-cache")
 ;; Initialization
-(setq cquery-extra-init-params '(:index (:comments 2) :cacheFormat "msgpack"))
+(setq cquery-extra-init-params '(:index (:comments 2) :cacheFormat "msgpack" :completion (:detailedLabel t)))
 
 (with-eval-after-load 'projectile
   (setq projectile-project-root-files-top-down-recurring
@@ -52,28 +50,26 @@
       (lsp-cquery-enable)
     (user-error nil)))
 
-(use-package cquery
-  :commands lsp-cquery-enable
-  :init (add-hook 'c-mode-common-hook #'cquery//enable))
-;; Also see lsp-project-whitelist lsp-project-blacklist cquery-root-matchers
+(lsp-cquery-enable)
+(add-hook 'c-mode-common-hook #'cquery//enable)
 
-(require-package 'helm-xref)
-(setq xref-show-xrefs-function 'helm-xref-show-xrefs)
+;;(use-package cquery
+;;  :commands lsp-cquery-enable
+;;  :init (add-hook 'c-mode-common-hook #'cquery//enable))
+;;;; Also see lsp-project-whitelist lsp-project-blacklist cquery-root-matchers
+;;
+
+(require-package 'ivy-xref)
+(setq xref-show-xrefs-function 'ivy-xref-show-xrefs)
+
 
 (require-package 'lsp-ui)
 (add-hook 'lsp-mode-hook 'lsp-ui-mode)
 (add-hook 'c-mode-common-hook 'flycheck-mode)
 ;;(setq lsp-ui-sideline-enable nil)
 (setq lsp-ui-doc-enable nil)
-(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-
-(setq cquery-sem-highlight-method 'font-lock)
-;;(setq cquery-sem-highlight-method 'overlay)
-
-;; For rainbow semantic highlighting
-(cquery-use-default-rainbow-sem-highlight)
-;;(setq lsp-highlight-symbol-at-point nil)
+;;(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+;;(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
 
 ;;(cquery-xref-find-custom "$cquery/base")
 ;;(cquery-xref-find-custom "$cquery/callers")
@@ -85,11 +81,18 @@
 ;;(lsp-ui-peek-find-custom 'callers "$cquery/callers")
 ;;(lsp-ui-peek-find-custom 'random "$cquery/random") ;; jump to a random declaration
 ;;
-(setq lsp-ui-doc-include-signature nil)  ; don't include type signature in the child frame
-(setq lsp-ui-sideline-show-symbol nil)  ; don't show symbol on the right of info
-
+;;(setq lsp-ui-doc-include-signature nil)  ; don't include type signature in the child frame
+;;(setq lsp-ui-sideline-show-symbol nil)  ; don't show symbol on the right of info
+;;
 ;;(cquery-call-hierarchy nil) ; caller hierarchy
 ;;(cquery-call-hierarchy t) ; callee hierarchy
+
+(setq cquery-sem-highlight-method 'font-lock)
+;;(setq cquery-sem-highlight-method 'overlay)
+
+;; For rainbow semantic highlighting
+(cquery-use-default-rainbow-sem-highlight)
+;;(setq lsp-highlight-symbol-at-point nil)
 
 (setq company-transformers nil company-lsp-async t company-lsp-cache-candidates nil)
 (setq cquery-extra-init-params '(:completion (:detailedLabel t)))
